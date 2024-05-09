6a781d5d69356cc0f138b96650e0e8089940b924
JJCondoriPinto <juanjosecondoripinto@gmail.com>
2024-05-09 10:28:16 -0500

    feat | definir enrutamiento de paginas

---
+++ src/ui/app/dashboard/pages/grupos/grupos.page.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'grupos-page',
+    templateUrl: './grupos.page.html',
+})
+export class GruposPage {}
---
+++ src/ui/app/dashboard/pages/usuarios/usuarios.page.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'usuarios-page',
+    templateUrl: './usuarios.page.html',
+})
+export class UsuariosPage {}
