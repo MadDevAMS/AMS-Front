11edb2c0845cf20ce658e56de354c5c164ac8685
Alejandro16232 <abel.ponce@tecsup.edu.pe>
2024-05-10 20:44:34 -0500

    feat|Implementar el tema de figma a angular

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,44 +6,43 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        Blue01:'#1B1C31',
-        Blue02:'#222338',
-        LightBlue:'#006AFF',
-        Cyan:'#15BFFD',
-        Green:'#52C93F',
-        Green2:'#009D92',
-        Red:'#FF2727',
-        Blood:'#DC3939',
-        Gray01:'#525256',
-        Gray02:'#656575',
-        Gray03:'#A3A3A3',
-        Gray04:'#A6ABC8',
-        Gray05:'#F8F7F1',
-        LightGreen01:'#C6EAFD',
-        LIghtGreen02:'#B2FFDB',
-        smoke1:'#F8F7F1',
-        smoke2:'#F4F5F7',
-        white:'#FDFDFD',
-        LinearLightGreen: {
-          '0%': '#00B2FF',
-          '14%': '#00B2FF',
-          '15%': '#15BFFD',
-          '40%': '#15BFFD',
-          '100%': '#15BFFD',
-        },
-        warning:'#FFD680',
-        warning2: '#FFA600',
+        'blue01':'#1B1C31',
+        'blue02':'#222338',
+        'light-blue':'#006AFF',
+        'cyan':'#15BFFD',
+        'green':'#52C93F',
+        'green2':'#009D92',
+        'red':'#FF2727',
+        'blood':'#DC3939',
+        'gray01':'#525256',
+        'gray02':'#656575',
+        'gray03':'#A3A3A3',
+        'gray04':'#A6ABC8',
+        'gray05':'#F8F7F1',
+        'light-green01':'#C6EAFD',
+        'light-green02':'#B2FFDB',
+        'smoke1':'#F8F7F1',
+        'smoke2':'#F4F5F7',
+        'white':'#FDFDFD',
+        'linear-light-green': '#00B2FF',
+        'warning':'#FFD680',
+        'warning2': '#FFA600',
       },
-      backgroundImage: theme => ({
-        'gradient-linear-light-green': `linear-gradient(to right, ${theme('colors.LinearLightGreen')['0%']}, ${theme('colors.LinearLightGreen')['14%']}, ${theme('colors.LinearLightGreen')['15%']}, ${theme('colors.LinearLightGreen')['40%']}, ${theme('colors.LinearLightGreen')['100%']})`,
-      }),
-
+      
+      boxShadow: {
+        'sm_ams': '0px 1px 2px 0 rgba(0 0 0 / 0.5)',
+        'md_ams': '0px 4px 6px -1px rgba(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
+        'lg_ams': '0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)',
+        'xl_ams': '0 -20px 25px -5px rgba(0 0 0 / 0.1), 0 10px 10px -5px rgba(0 0 0/ 0.04)',
+        
+      },
+      fontFamily: {
+        regular_poppins: ['Poppins','sans-serif','Helvetica'],
+        regular_inter: ['Inter','sans-serif','Helvetica']
+      },
     },
   },
-  fontFamily: {
-    poppins: ['Poppins', 'sans-serif'],
-    inter: ['Inter', 'sans-serif'],
-  },
+  
   plugins: [],
 }
 
