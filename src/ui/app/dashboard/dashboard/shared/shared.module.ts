c5bc8b8a4277626fdf441060bd24c7d8f847984d
JJCondoriPinto <juanjosecondoripinto@gmail.com>
2024-06-06 16:17:15 -0500

    feat: cartas dashboard

--- src/ui/app/dashboard/dashboard/shared/shared.module.ts
+++ src/ui/app/dashboard/dashboard/shared/shared.module.ts
@@ -2,18 +2,26 @@ import { CdkTreeModule } from '@angular/cdk/tree';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from '@ui/shared/modules/material.module';
 import { SharedGlobalModule } from '@ui/shared/modules/shared-global.module';
+import { InicidenciasListComponent } from './components/incidencias/incidencias-list.component';
+import { incidenciaComponent } from './components/incidencias/incidencia.component';
+
+const components = [
+  incidenciaComponent,
+  InicidenciasListComponent,
+]
+
+const common = [
+  MaterialModule,
+  SharedGlobalModule,
+  CdkTreeModule
+]
 
 @NgModule({
-  declarations: [],
-  imports: [ 
-    MaterialModule,
-    SharedGlobalModule,
-    CdkTreeModule
-  ],
+  declarations: components,
+  imports: common,
   exports: [
-    MaterialModule,
-    SharedGlobalModule,
-    CdkTreeModule
+    ...common,
+    ...components
   ],
   providers: [],
 })
