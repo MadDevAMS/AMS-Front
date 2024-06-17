b77b66eea8ddce5a42cfcc58867f38839225417b
Alejandro16232 <abel.ponce@tecsup.edu.pe>
2024-06-17 10:14:18 -0500

    Implementar vista de monitoreo para motores

--- src/ui/app/dashboard/dashboard/monitoreo/tabla/tabla.component.ts
+++ src/ui/app/dashboard/dashboard/monitoreo/tabla/tabla.component.ts
@@ -3,11 +3,11 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-tabla',
   templateUrl: './tabla.component.html',
+  styleUrls: ['./tabla.component.css']
 })
 export class TablaComponent {
 maquinaSeleccionada: string | null = null;
 
-  // Método ficticio para seleccionar una máquina
   seleccionarMaquina(id: string) {
     this.maquinaSeleccionada = id;
   }
