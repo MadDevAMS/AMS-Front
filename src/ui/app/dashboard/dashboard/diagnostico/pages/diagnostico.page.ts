ca8a02b742a3a46a9650d91911a70e702d56d74c
JJCondoriPinto <juanjosecondoripinto@gmail.com>
2024-06-25 19:08:30 -0500

    feat: encabezados y pagina de diagnostico

--- src/ui/app/dashboard/dashboard/diagnostico/pages/diagnostico.page.ts
+++ src/ui/app/dashboard/dashboard/diagnostico/pages/diagnostico.page.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
 
 @Component({
   selector: 'diagnostico-page',
   standalone: true,
+  imports: [
+    SharedModule,
+    
+  ],
   templateUrl: './diagnostico.page.html',
 })
 export class DiagnosticoPage {
