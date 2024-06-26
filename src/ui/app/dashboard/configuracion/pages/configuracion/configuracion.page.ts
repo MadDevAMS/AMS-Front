f713cee2e99e36ecb5312fd109f456bad04bf28a
KatherineMilagros <katherine.quispr@tecsup.edu.pe>
2024-06-25 19:49:42 -0500

    cambios

--- src/ui/app/dashboard/configuracion/pages/configuracion/configuracion.page.ts
+++ src/ui/app/dashboard/configuracion/pages/configuracion/configuracion.page.ts
@@ -1,7 +1,39 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
-
 @Component({
-  selector: 'configuracion-page',
+  selector: 'app-configuracion',
   templateUrl: './configuracion.page.html',
+  styleUrls: ['./configuracion.page.css']
 })
-export class ConfiguracionPage {}
+export class ConfiguracionPage {
+  constructor(private router: Router) {}
+
+  redirectToAuth() {
+    this.router.navigate(['/auth']);
+  }
+
+  toggleTheme() {
+    console.log('Tema cambiado');
+  }
+
+  openUserConfig() {
+    console.log('Configuración del usuario abierta');
+  }
+
+  viewAccountInfo() {
+    console.log('Información de la cuenta vista');
+  }
+
+  changePassword() {
+    console.log('Contraseña cambiada');
+  }
+
+  logout() {
+    this.router.navigate(['/auth']);
+    localStorage.removeItem('token');
+  }
+
+  deleteAccount() {
+    console.log('Cuenta eliminada');
+  }
+}
